8a0917dfb396d86a74759d8ec3f82f3c4b434e68
jihami <w2027@e-mirim.hs.kr>
2021-10-25 20:27:04 +0900

    오타 수정,,,

--- pygame_basic/1_crate_frame.py
+++ pygame_basic/1_crate_frame.py
@@ -4,8 +4,8 @@ pygame.init() #초기화 (반드시 필요)
 
 #화면 크기 설정
 screen_width = 480 #가로 크기
-screen_high = 640 #세로 크기
-screen = pygame.display.set_mode((screen_width, screen_high))
+screen_height = 640 #세로 크기
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 #화면 타이틀 설정
 pygame.display.set_caption("Hippo Game") #게임이름
